c8dce02400f829833effdd8c40effe79e8628cb6
Pablo-Arranz <pabloarranz9@gmail.com>
2022-09-11 11:03:47 -0500

    Create initial files

---
+++ etch-a-sketch.js
@@ -0,0 +1,4 @@
+// create 16x16 grid of square divs
+// place them inside another container
+// grids change color when you hover over them
+// add a button that will send user a popup asking for number of squares per side
